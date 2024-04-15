4c86efd01643953fb2ac1f384fcb0505d0b874c6
DimiON112 <dmitrosegeda@gmail.com>
2024-04-15 17:34:12 +0200

    test

--- e-cook.client/vite.config.js
+++ e-cook.client/vite.config.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
-//test   
+//test      a
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
         ? `${env.APPDATA}/ASP.NET/https`
@@ -32,7 +32,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7174';
-
+    //test
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
