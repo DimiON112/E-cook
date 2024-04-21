f876785ed52f7af48db6f455631a28adbb9ccd11
Yaraslau <yaroslav-sinkevich@inbox.ru>
2024-04-21 16:25:17 +0200

    dodalem poczatek poszukiwania

--- PHPAccount/assets/js/home.js
+++ PHPAccount/assets/js/home.js
@@ -1,7 +1,22 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 "use strict";
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+    if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+
+/** */
+
+$searchField.addEventListener("keydown", e => {
+    if (e.key === "Enter") $searchBtn.click();
+});
+
+/** */
+const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let /** {NodeElement} */[$lastActiveTabPanel] = $tabPanels;
+let /** {NodeElement} */[$lastActiveTabBtn] = $tabBtns;
