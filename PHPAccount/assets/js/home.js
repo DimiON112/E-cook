5eea4e02e14ca1141fdb1ed494a54bdc00551a26
Masza <mariia.lozovikova@gmail.com>
2024-04-23 22:58:39 +0200

    tab, slider, tag section

--- PHPAccount/assets/js/home.js
+++ PHPAccount/assets/js/home.js
@@ -20,3 +20,41 @@ const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]
 
 let /** {NodeElement} */[$lastActiveTabPanel] = $tabPanels;
 let /** {NodeElement} */[$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, "click", function () {
+    $lastActiveTabPanel.setAttribute("hidden", "");
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabBtn.setAttribute("tabindex", -1);
+  
+    const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", 0);
+  
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+  
+    addTabContent(this, $currentTabPanel);
+  });
+
+  addEventOnElements($tabBtns, "keydown", function (e) {
+
+    const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+    const /** {NodeElement} */ $previousElement = this.previousElementSibling;
+  
+    if (e.key === "ArrowRight" && $nextElement) {
+      this.setAttribute("tabindex", -1);
+      $nextElement.setAttribute("tabindex", 0);
+      $nextElement.focus();
+    } else if (e.key === "ArrowLeft" && $previousElement) {
+      this.setAttribute("tabindex", -1);
+      $previousElement.setAttribute("tabindex", 0);
+      $previousElement.focus();
+    } else if (e.key === "Tab") {
+      this.setAttribute("tabindex", -1);
+      $lastActiveTabBtn.setAttribute("tabindex", 0);
+    }
+  
+  });
+
+  
