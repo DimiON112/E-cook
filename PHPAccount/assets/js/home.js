ec50f71023c899a15c31fd919ff3a170a0d66851
Yaraslau <yaroslav-sinkevich@inbox.ru>
2024-04-29 11:00:24 +0200

    przywijanie+ulub

--- PHPAccount/assets/js/home.js
+++ PHPAccount/assets/js/home.js
@@ -4,7 +4,7 @@ const /** {NodeElement} */ $searchField = document.querySelector("[data-search-f
 const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn]");
 
 $searchBtn.addEventListener("click", function () {
-    if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+  if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
 });
 
 
@@ -18,7 +18,7 @@ import { getTime } from "./module.js";
 
 
 $searchField.addEventListener("keydown", e => {
-    if (e.key === "Enter") $searchBtn.click();
+  if (e.key === "Enter") $searchBtn.click();
 });
 
 /** */
@@ -29,43 +29,43 @@ let /** {NodeElement} */[$lastActiveTabPanel] = $tabPanels;
 let /** {NodeElement} */[$lastActiveTabBtn] = $tabBtns;
 
 addEventOnElements($tabBtns, "click", function () {
-    $lastActiveTabPanel.setAttribute("hidden", "");
-    $lastActiveTabBtn.setAttribute("aria-selected", false);
-    $lastActiveTabBtn.setAttribute("tabindex", -1);
-  
-    const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
-    $currentTabPanel.removeAttribute("hidden");
-    this.setAttribute("aria-selected", true);
-    this.setAttribute("tabindex", 0);
-  
-    $lastActiveTabPanel = $currentTabPanel;
-    $lastActiveTabBtn = this;
-  
-    addTabContent(this, $currentTabPanel);
-  });
+  $lastActiveTabPanel.setAttribute("hidden", "");
+  $lastActiveTabBtn.setAttribute("aria-selected", false);
+  $lastActiveTabBtn.setAttribute("tabindex", -1);
 
-  addEventOnElements($tabBtns, "keydown", function (e) {
+  const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+  $currentTabPanel.removeAttribute("hidden");
+  this.setAttribute("aria-selected", true);
+  this.setAttribute("tabindex", 0);
 
-    const /** {NodeElement} */ $nextElement = this.nextElementSibling;
-    const /** {NodeElement} */ $previousElement = this.previousElementSibling;
-  
-    if (e.key === "ArrowRight" && $nextElement) {
-      this.setAttribute("tabindex", -1);
-      $nextElement.setAttribute("tabindex", 0);
-      $nextElement.focus();
-    } else if (e.key === "ArrowLeft" && $previousElement) {
-      this.setAttribute("tabindex", -1);
-      $previousElement.setAttribute("tabindex", 0);
-      $previousElement.focus();
-    } else if (e.key === "Tab") {
-      this.setAttribute("tabindex", -1);
-      $lastActiveTabBtn.setAttribute("tabindex", 0);
-    }
-  
-  });
+  $lastActiveTabPanel = $currentTabPanel;
+  $lastActiveTabBtn = this;
+
+  addTabContent(this, $currentTabPanel);
+});
+
+addEventOnElements($tabBtns, "keydown", function (e) {
+
+  const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+  const /** {NodeElement} */ $previousElement = this.previousElementSibling;
+
+  if (e.key === "ArrowRight" && $nextElement) {
+    this.setAttribute("tabindex", -1);
+    $nextElement.setAttribute("tabindex", 0);
+    $nextElement.focus();
+  } else if (e.key === "ArrowLeft" && $previousElement) {
+    this.setAttribute("tabindex", -1);
+    $previousElement.setAttribute("tabindex", 0);
+    $previousElement.focus();
+  } else if (e.key === "Tab") {
+    this.setAttribute("tabindex", -1);
+    $lastActiveTabBtn.setAttribute("tabindex", 0);
+  }
+
+});
 
 
-  
+
 /**
  * WORK WITH API
  * fetch data for tab content
@@ -150,4 +150,97 @@ const addTabContent = ($currentTabBtn, $currentTabPanel) => {
 }
 
 addTabContent($lastActiveTabBtn, $lastActiveTabPanel);
-  
+
+
+let /** {Array} */ cuisineType = ["Asian", "French"];
+
+const /** {NodeList} */ $sliderSections = document.querySelectorAll("[data-slider-section]");
+
+for (const [index, $sliderSection] of $sliderSections.entries()) {
+
+  $sliderSection.innerHTML = `
+    <div class="container">
+      <h2 class="section-title headline-small" id="slider-label-1">Latest ${cuisineType[index]} Recipes</h2>
+
+      <div class="slider">
+        <ul class="slider-wrapper" data-slider-wrapper>
+          ${`<li class="slider-item">${$skeletonCard}</li>`.repeat(10)}
+        </ul>
+      </div>
+    </div>
+  `;
+
+
+  const /** {NodeElement} */ $sliderWrapper = $sliderSection.querySelector("[data-slider-wrapper]");
+
+  fetchData([...cardQueries, ["cuisineType", cuisineType[index]]], function (data) {
+
+    $sliderWrapper.innerHTML = "";
+
+    data.hits.map(item => {
+
+      const {
+        recipe: {
+          image,
+          label: title,
+          totalTime: cookingTime,
+          uri
+        }
+      } = item;
+
+      const /** {String} */ recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+      const /** {undefined || String} */ isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`);
+
+      const /** {NodeElement} */ $sliderItem = document.createElement("li");
+      $sliderItem.classList.add("slider-item");
+
+      $sliderItem.innerHTML = `
+        <div class="card">
+          <figure class="card-media img-holder">
+            <img src="${image}" width="195" height="195" loading="lazy" alt="${title}"
+              class="img-cover">
+          </figure>
+
+          <div class="card-body">
+
+            <h3 class="title-small">
+              <a href="./detail.html?recipe=${recipeId}" class="card-link">${title ?? "Untitled"}</a>
+            </h3>
+
+            <div class="meta-wrapper">
+
+              <div class="meta-item">
+                <span class="material-symbols-outlined" aria-hidden="true">schedule</span>
+
+                <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}</span>
+              </div>
+
+              <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')">
+                <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+
+                <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+              </button>
+
+            </div>
+
+          </div>
+        </div>
+      `;
+
+      $sliderWrapper.appendChild($sliderItem);
+
+    });
+
+    $sliderWrapper.innerHTML += `
+      <li class="slider-item" data-slider-item>
+        <a href="./recipes.html?cuisineType=${cuisineType[index].toLowerCase()}" class="load-more-card has-state">
+          <span class="label-large">Show more</span>
+
+          <span class="material-symbols-outlined" aria-hidden="true">navigate_next</span>
+        </a>
+      </li>
+    `;
+
+  });
+
+}
